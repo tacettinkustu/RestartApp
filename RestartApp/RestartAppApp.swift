1a7378f74189c1f77396345388d458c4e174fa3f
Tacettin Kustu <tacettinkustu@gmail.com>
2023-05-26 18:37:36 +0300

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Tacettin Küstü on 26.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
