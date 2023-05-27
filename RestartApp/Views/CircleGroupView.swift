82df8e78634a5ce3767b754c358047dba5bbf8c3
Tacettin Kustu <tacettinkustu@gmail.com>
2023-05-27 13:43:14 +0300

    Add animation into circleGroupView

--- RestartApp/Views/CircleGroupView.swift
+++ RestartApp/Views/CircleGroupView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct CircleGroupView: View {
     @State var ShapeColor: Color
     @State var ShapeOpacity: Double
+    @State private var isAnimating: Bool = false
     
     var body: some View {
         ZStack {
@@ -20,6 +21,13 @@ struct CircleGroupView: View {
                 .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
                 .frame(width: 260, height: 260, alignment: .center)
         } //: ZStack
+        .blur(radius: isAnimating ? 0 : 10)
+        .opacity(isAnimating ? 1 : 0)
+        .scaleEffect(isAnimating ? 1 : 0.5)
+        .animation(.easeOut(duration: 1), value: isAnimating)
+        .onAppear {
+            isAnimating = true
+        }
     }
 }
 
