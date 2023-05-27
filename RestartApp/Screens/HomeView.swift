76fe81a6839a931056928ea2ae70c5d962576d57
Tacettin Kustu <tacettinkustu@gmail.com>
2023-05-27 10:03:31 +0300

    Complete implementation of design into homeView screen

--- RestartApp/Screens/HomeView.swift
+++ RestartApp/Screens/HomeView.swift
@@ -13,15 +13,37 @@ struct HomeView: View {
     
     var body: some View {
             VStack(spacing: 20, content: {
-                Text("Home")
-                    .font(.largeTitle)
+                
+                Spacer()
+                
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                
+                Text("The time that leads to mastery is depend on the intensity of our focus.")
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.secondary)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                
+                Spacer()
+                
                 Button {
                     isOnboardingViewActive = true
                 } label: {
+                    Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                        .imageScale(.large)
+                    
                     Text("Restart")
-                }
-                
-            }) // VStack
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                } //: Button
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.capsule)
+                .controlSize(.large)
+            }) //: VStack
     }
 }
 
