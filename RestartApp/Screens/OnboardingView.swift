023d3e833cc923919d2340bc77415b5fb530920c
Tacettin Kustu <tacettinkustu@gmail.com>
2023-05-27 16:09:34 +0300

    Add horizontal parallax effect

--- RestartApp/Screens/OnboardingView.swift
+++ RestartApp/Screens/OnboardingView.swift
@@ -14,6 +14,7 @@ struct OnboardingView: View {
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0
     @State private var isAnimating: Bool = false
+    @State private var imageOffset: CGSize = .zero
     
     var body: some View {
         ZStack{
@@ -46,12 +47,29 @@ struct OnboardingView: View {
                 
                 ZStack {
                     CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+                        .offset(x: imageOffset.width * -1)
+                        .blur(radius: abs(imageOffset.width / 5))
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                     
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
                         .opacity(isAnimating ? 1 : 0)
                         .animation(.easeOut(duration: 0.6), value: isAnimating)
+                        .offset(x: imageOffset.width * 1.2, y: 0)
+                        .rotationEffect(.degrees(Double(imageOffset.width / 20)))
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ gesture in
+                                    if abs(imageOffset.width) <= 150 {
+                                        imageOffset = gesture.translation
+                                    }
+                                })
+                                .onEnded({ _ in
+                                    imageOffset = .zero
+                                })
+                        ) //: Gesture
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                 } //: Center
                 
                 Spacer()
